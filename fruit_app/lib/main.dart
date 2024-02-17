e599515784a806a3298dd8cda6ee81dba5f55af6
Vanuja Sooriyaarachchi <146374517+VanujaSooriyaarachchi@users.noreply.github.com>
2024-02-17 22:10:18 +0530

    Update background

--- fruit_app/lib/main.dart
+++ fruit_app/lib/main.dart
@@ -31,56 +31,74 @@ class HomePage extends StatelessWidget {
         title: const Text('Home Page'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Card(
-              elevation: 5,
-              margin: const EdgeInsets.all(20),
-              child: Container(
-                padding: const EdgeInsets.all(20),
-                width: 300,
-                child: const Column(
-                  children: [
-                    Text(
-                      'Welcome to the',
-                      style: TextStyle(fontSize: 20),
+      body: Stack(
+        children: <Widget>[
+          // Background Image
+          Image.asset(
+            'assets/fruits.jpg', // Replace with the actual path to your image
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          // Background Container with Opacity
+          Container(
+            color: Color.fromARGB(255, 90, 80, 80).withOpacity(0.5), // Set your desired color and opacity
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Card(
+                  elevation: 5,
+                  margin: const EdgeInsets.all(20),
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    width: 300,
+                    child: const Column(
+                      children: [
+                        Text(
+                          'Welcome to the',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          'Fruit Ripeness Detection System!',
+                          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
                     ),
-                    SizedBox(height: 10),
-                    Text(
-                      'Fruit Ripeness Detection System!',
-                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
+                  ),
                 ),
-              ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const SecondPage()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                    onPrimary: Colors.white,
+                    padding: const EdgeInsets.all(15),
+                  ),
+                  child: const Text(
+                    'Get Started',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SecondPage()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.blue,
-                onPrimary: Colors.white,
-                padding: const EdgeInsets.all(15),
-              ),
-              child: const Text(
-                'Get Started',
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
+
 
 class SecondPage extends StatefulWidget {
   const SecondPage({Key? key}) : super(key: key);
