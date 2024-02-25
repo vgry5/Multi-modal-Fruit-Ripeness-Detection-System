020e164c77c17394b4aced8f06e9189507a6a392
RMAvishka <129612849+RMAvishka@users.noreply.github.com>
2024-02-26 00:25:39 +0530

    Update main.dart
    change the design of ui

--- main.dart
+++ main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; //this package for exit button
+import 'package:flutter_application_vs5/SecondScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,7 +26,8 @@ class MyApp extends StatelessWidget {
               left: 25,
               child: ElevatedButton(
                 onPressed: () {
-                  // Code to be executed when the top left button is pressed
+                  SystemNavigator.pop();
+                  // Code to be executed when the Exit button is pressed
                   print('Exit Button pressed');
                 },
                 child: Text('Exit'),
@@ -37,6 +40,11 @@ class MyApp extends StatelessWidget {
               left: 150,
               child: ElevatedButton(
                 onPressed: () {
+                  // Navigate to the SecondScreen when the button is pressed
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SecondScreen()),
+                  );
                   // Code to be executed when the bottom center button is pressed
                   print('Predit Again Button pressed');
                 },
@@ -49,7 +57,7 @@ class MyApp extends StatelessWidget {
               bottom: 400, //postioned have top,bottom,right,left,height,width
               left: 90, //bottom:50, /n  right:25
               child: Text(
-                'Your Ripness level is \n......................',
+                'Your Ripness level is \n...........................\n.                             .\n.                             .\n.                             .\n.                             .\n...........................',
                 style: TextStyle(
                   fontSize: 24.0,
                   color: Colors.black,
@@ -62,7 +70,7 @@ class MyApp extends StatelessWidget {
               bottom: 200,
               left: 90,
               child: Text(
-                '............... \n...........',
+                '............... \n...............',
                 style: TextStyle(
                   fontSize: 24.0,
                   color: Colors.black,
