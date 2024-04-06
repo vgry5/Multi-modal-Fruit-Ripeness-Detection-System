de8204e511ec60687e981264a64f0a31fb221f71
Vanuja Sooriyaarachchi <146374517+VanujaSooriyaarachchi@users.noreply.github.com>
2024-04-06 22:44:25 +0530

    Change main

--- Fruit Application/test/widget_test.dart
+++ Fruit Application/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fruit_app/mainPage.dart';
+import 'package:fruit_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
